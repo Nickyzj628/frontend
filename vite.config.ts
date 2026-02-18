be090f888738ccc5417ca62543d3783ac784a587
Nickyzj <1927139831@qq.com>
2026-02-18 12:06:13 +0800

    refactor: 把原先的 vite-bundle-analyzer 替换为 rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import path from "node:path";
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
-import { analyzer } from "vite-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
 	plugins: [
 		preact(),
 		tailwindcss(),
-		analyzer({
-			enabled: command === "build",
-			defaultSizes: "gzip",
+		visualizer({
+			open: true, // 构建后自动打开浏览器
+			gzipSize: true, // 显示 gzip 大小
+			filename: "dist/stats.html", // 输出路径（建议放 dist 方便部署）
+			template: "treemap", // 可选: treemap/sunburst/network
 		}),
 	],
 	resolve: {
